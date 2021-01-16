919ed58daabb0c5c0b515be7feefbfb533915bbb
Alex Zero <joseph@marsden.space>
2021-01-16 22:15:59 +0000

    fix: remove shit that accidentally got committed

--- users/alex/develop/languages/python/default.nix
+++ users/alex/develop/languages/python/default.nix
@@ -4,7 +4,7 @@ let
     python = pkgs.python3Full.withPackages packages;
 in
 {
-    home.packages = with pkgs; [ python conda ];
+    home.packages = with pkgs; [ python conda jetbrains.pycharm-professional ];
     programs.vscode.userSettings.python = {
         languageServer = "Pylance";
     };
