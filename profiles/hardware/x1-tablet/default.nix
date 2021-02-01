a8247dbc51981471b18061a7ae4990b406d4dd77
Alex Zero <joseph@marsden.space>
2021-02-01 16:43:23 +0000

    fix: revert custom power button behaviour

--- profiles/hardware/x1-tablet/default.nix
+++ profiles/hardware/x1-tablet/default.nix
@@ -1,14 +1,4 @@
 { pkgs, ... }:
-
-let
-    screenLockScript = pkgs.writeShellScript "screen-lock.sh" ''
-        display=":$(ls /tmp/.X11-unix/* | sed 's#/tmp/.X11-unix/X##' | tail -n 1)"
-        user="$(who | grep '('$display')' | ${pkgs.gawk}/bin/awk '{print $1}' | head -n 1)"
-        uid="$(id -u $user)"
-        ${pkgs.sudo}/bin/sudo -u $user DISPLAY=$display DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$uid/bus \
-            ${pkgs.dbus}/bin/dbus-send --type=method_call --dest=org.gnome.ScreenSaver /org/gnome/ScreenSaver org.gnome.ScreenSaver.Lock
-    '';
-in
 {
     boot = {
         # we're hidpi so use lower res for boot
@@ -32,22 +22,6 @@ in
     services.logind = {
         # properly sleep on lid close
         lidSwitchDocked = "suspend";
-
-        # make sure we ignore buttons
-        # as we're doing things our own way
-        extraConfig = ''
-            HandlePowerKey=ignore
-            HandleSuspendKey=ignore
-            HandleHibernateKey=ignore
-            HandleRebootKey=ignore
-        '';
-    };
-
-    services.acpid = {
-        enable = true;
-        powerEventCommands = ''
-            ${screenLockScript}
-        '';
     };
 
     # enable display hidpi
