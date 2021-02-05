2acad0086e3f591dfe92da98234c495db85f6684
Alex Zero <joseph@marsden.space>
2021-02-05 04:30:25 +0000

    feat: add experimental hardware configuration

--- profiles/hardware/x1-tablet/default.nix
+++ profiles/hardware/x1-tablet/default.nix
@@ -4,20 +4,40 @@
         # we're hidpi so use lower res for boot
         loader.systemd-boot.consoleMode = "1";
 
-        # use latest kernel version
-        kernelPackages = pkgs.linuxPackages_5_10;
+        # use latest stable kernel version
+        kernelPackages = pkgs.linuxPackages_5_9;
         kernelPatches = [
-            # fix for the keyboard function keys
             {
                 name = "tpx1-cover";
                 patch = ./tpx1-cover.patch;
             }
+            # {
+            #     name = "ov8858-camera";
+            #     patch = ./ov8858-camera.patch;
+            #     extraConfig = ''
+            #         PMIC_OPREGION y
+            #         STAGING_MEDIA y
+            #         INTEL_ATOMISP y
+            #         VIDEO_ATOMISP m
+            #         VIDEO_ATOMISP_IMX m
+            #         VIDEO_ATOMISP_OV8858 m
+            #         VIDEO_ATOMISP_MSRLIST_HELPER m
+            #         VIDEO_IPU3_IMGU m
+            #     '';
+            # }
         ];
-    };
 
-    #sound.extraConfig = ''
-    #    options snd-hda-intel model=laptop-dmic
-    #'';
+        # enable this block for kernel module iteration
+        # must build with i.e `sudo ./cursed-rebuild switch --impure`
+        # kernelPackages = let kernel = (pkgs.linuxManualConfig {
+        #     inherit (pkgs) stdenv;
+        #     version = "5.9.0";
+
+        #     src = /home/alex/src/external/kernel/linux;
+        #     configfile = /home/alex/src/external/kernel/kernel.conf;
+        #     allowImportFromDerivation = true;
+        # }); in pkgs.linuxPackagesFor kernel;
+    };
 
     hardware = {
         video.hidpi.enable = true;
@@ -37,6 +57,14 @@
         logind = {
             # properly sleep on lid close
             lidSwitchDocked = "suspend";
+        };
+
+        # undervolt cpu and igpu
+        # units are in mV (milivolts)
+        undervolt = {
+            enable = true;
+            coreOffset = -95;
+            gpuOffset = -48;
         };
     };
 }
