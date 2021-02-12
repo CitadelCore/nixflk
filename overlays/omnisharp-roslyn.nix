bd3b583e28c17355ab28bef6029bf45b8c814c3d
Alex Zero <joseph@marsden.space>
2021-02-12 12:42:05 +0000

    fix: un-fuck & restore C# and omnisharp

--- overlays/omnisharp-roslyn.nix
+++ overlays/omnisharp-roslyn.nix
@@ -1,8 +1,10 @@
 final: prev: with prev; {
     omnisharp-roslyn = prev.omnisharp-roslyn.overrideAttrs(o: {
-        src = fetchurl {
-            url = "https://arc-extern.s3.eu-west-2.amazonaws.com/omnisharp-mono.tar.gz";
-            sha256 = "sha256-OC4z3Fv+YSN+rdO4gZAHyPbgGOr8TD/24XNIfJvIS2w=";
+        src = fetchFromGitHub {
+            owner = "CitadelCore";
+            repo = "omnisharp-roslyn";
+            rev = "master";
+            sha256 = "sha256-aZwrsIRidKK815x6/HpurQ5FyGPn7kYKQRLrowXMGlU=";
         };
     });
 }
