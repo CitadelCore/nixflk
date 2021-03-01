8722255ccf4480c0d0cb69d50d5a6c86c0f4c47b
Alex Zero <joseph@marsden.space>
2021-03-01 17:32:37 +0000

    fix: use sgx dcap for p15v

--- profiles/hardware/p15v/default.nix
+++ profiles/hardware/p15v/default.nix
@@ -1,5 +1,9 @@
+{ pkgs, ... }:
 {
     imports = [
         ../capabilities/fingerprint
     ];
+
+    # use the newer DCAP SGX driver because we have FLC support
+    security.sgx.packages.driver = pkgs.linuxPackages.intel-sgx-dcap;
 }
