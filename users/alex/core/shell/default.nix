e2457fe619bde1350bec0ec9bb959b8ad2f289b5
Alex Zero <joseph@marsden.space>
2021-08-21 21:54:54 +0100

    feat: add wsl ssh-agent support for fish

--- users/alex/core/shell/default.nix
+++ users/alex/core/shell/default.nix
@@ -59,7 +59,31 @@ in {
                 set -x TERM xterm
 
                 set -x XDG_DATA_DIRS "$HOME/.nix-profile/share:$XDG_DATA_DIRS"
-            '' + (optionalString (!my.wsl) ''
+            '' + (if my.wsl then ''
+                set -x SSH_AUTH_SOCK "$HOME/.ssh/agent.sock"
+                if not ss -a | grep -q "$SSH_AUTH_SOCK";
+                rm -f "$SSH_AUTH_SOCK"
+                set wsl2_ssh_pageant_bin "$HOME/.ssh/wsl2-ssh-pageant.exe"
+                if test -x "$wsl2_ssh_pageant_bin";
+                    setsid nohup socat UNIX-LISTEN:"$SSH_AUTH_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin" >/dev/null 2>&1 &
+                else
+                    echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+                end
+                set --erase wsl2_ssh_pageant_bin
+                end
+
+                set -x GPG_AGENT_SOCK "$HOME/.gnupg/S.gpg-agent"
+                if not ss -a | grep -q "$GPG_AGENT_SOCK";
+                rm -rf "$GPG_AGENT_SOCK"
+                set wsl2_ssh_pageant_bin "$HOME/.ssh/wsl2-ssh-pageant.exe"
+                if test -x "$wsl2_ssh_pageant_bin";
+                    setsid nohup socat UNIX-LISTEN:"$GPG_AGENT_SOCK,fork" EXEC:"$wsl2_ssh_pageant_bin --gpg S.gpg-agent" >/dev/null 2>&1 &
+                else
+                    echo >&2 "WARNING: $wsl2_ssh_pageant_bin is not executable."
+                end
+                set --erase wsl2_ssh_pageant_bin
+                end
+            '' else ''
                 set -x SSH_AUTH_SOCK "/run/user/$UID/gnupg/S.gpg-agent.ssh"
             '');
 
