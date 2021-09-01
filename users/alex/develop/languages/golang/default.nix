0eadd8882c9a882f0780762acb45d34c28a055ad
Alex Zero <joseph@marsden.space>
2021-09-01 19:06:44 +0100

    fix: set GOPATH

--- users/alex/develop/languages/golang/default.nix
+++ users/alex/develop/languages/golang/default.nix
@@ -1,5 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, my, ... }:
 {
-    home.packages = with pkgs; [ go delve ];
+    home = let
+        goPath = "/home/${my.username}/go";
+    in {
+        sessionPath = [ "${goPath}/bin" ];
+        sessionVariables."GOPATH" = goPath;
+        packages = with pkgs; [ go delve ];
+    };
+
     programs.vscode.userSettings.go.useLanguageServer = true;
 }
